8f644ca06f2da60af0627b12d0e0cce32ef29364
dandylion99 <446956866@qq.com>
2022-09-20 21:01:38 -0400

    Revise functions to make it the same as homework assignment description

--- solution-hw3/js/bakery.js
+++ solution-hw3/js/bakery.js
@@ -1,37 +1,26 @@
-function getGlazingPrice(){
-    let glazingElement = document.querySelector("#glazing");
-    let glazingNum = glazingElement.value;
+function glazingChange(element){
+    const glazingChange = element.value;
     const glazingList = {
         "Keep Original": 0,
         "Sugar milk": 0,
         "Vanilla milk": 0.5,
         "Double chocolate": 1.5 
     };
-    return glazingList[glazingNum];
-}
-
-function getPackPrice(){
-    let packElement = document.querySelector("#pack");
-    let packNum = packElement.value;
-    const packList = {
-        "1": 1,
-        "3": 3,
-        "6": 5,
-        "12": 10
-    }
-    // console.log(packList[packNum]);
-    return packList[packNum];
-}
-
-function glazingChange(){
-    glazingPrice = getGlazingPrice();
+    glazingPrice = glazingList[glazingChange];
     // check current price change
     // console.log("glazingprice: "+glazingPrice+", packPrice "+packPrice);
     updatePrice();
 }
 
-function packChange(){
-    packPrice = getPackPrice();
+function packChange(element){
+    const packChange = element.value;
+    const packList = {
+        "1": 1,
+        "3": 3,
+        "6": 5,
+        "12": 10
+    };
+    packPrice = packList[packChange];
     // check current price change
     // console.log("glazingprice: "+glazingPrice+", packPrice "+packPrice);
     updatePrice();
@@ -43,9 +32,8 @@ function updatePrice(){
     let priceElement = document.querySelector("#totalPrice");
     // console.log("totalPrice: "+totalPrice);
     priceElement.innerText = "$ "+totalPrice.toFixed(2);
-
 }
 
 // store glazingPrice and packPrice as global variable
-let glazingPrice = getGlazingPrice();
-let packPrice = getPackPrice();
+let glazingPrice = 0; //the default value from detail.html
+let packPrice = 1;
