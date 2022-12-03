b97b556a73f93030537aa1688fb247bfcca626d8
dandylion99 <446956866@qq.com>
2022-12-03 15:41:47 -0500

    Complete major animations

--- project-test/js/app.js
+++ project-test/js/app.js
@@ -1,2 +1,186 @@
-let movElement = document.querySelector("#impressionism")
-// movElement.addEventListener("click", expandAnimation)
+class Painting {
+    ID;
+    name;
+    img;
+    artist;
+    year;
+    intro;
+    artist_year;
+    artist_img;
+    artist_intro;
+
+    constructor(ID) {
+        this.ID = ID;
+   }
+}
+
+//reference: https://stackoverflow.com/questions/24681127/play-animation-then-load-next-page
+if (document.URL.includes("index")){
+    $(document).ready(function() {
+        const fade_out = [
+            'animate__animated',
+            'animate__fadeOut'
+          ];
+          const fade_in = [
+            'animate__animated',
+            'animate__fadeIn'
+          ];
+        const zoom_in = [
+            'animate__animated',
+            'animate__zoomIn'
+        ];
+      
+        $("#Neoclassicism").click(function() {
+            $(".timeline-bar")
+                .addClass(zoom_in);
+            $(".intro-container")
+                .addClass(fade_out);
+            $(".movement-container")
+                .addClass(fade_out)
+                .bind("animationend webkitAnimationEnd oAnimationEnd MSAnimationEnd",
+                    function() {
+                    window.location.href = "movement.html"; 
+                });
+        });
+
+        // $("#Neoclassicism").mouseover(function(){
+        //     $('.background')
+        //         .removeClass("fade-out")
+        //         .addClass("fade-in");
+        
+
+        //     // $('body,html').css({backgroundColor: 'red'})
+        // });
+
+        // $("#Neoclassicism").mouseleave(function(){
+        //     $('.background')
+        //         .removeClass("fade-in")
+        //         .addClass("fade-out");
+        //     // $('body,html').css({backgroundColor: 'red'})
+        // });
+
+        $(".movement-box").mouseover(function(){
+            let img = $(this).attr('id');
+
+            img = movements[img]['img'];
+
+            let imgURL = "url(assets/" + img + ")";
+            console.log(imgURL);
+            $('.background')
+                .css("background-image",imgURL)
+                .removeClass("fade-out")
+                .addClass("fade-in");
+                // .css("background-image",'url("assets/Impression_Sunrise.png")');
+        });
+
+        $(".movement-box").mouseleave(function(){
+            $('.background')
+                // .css("background-image","")
+                .removeClass("fade-in")
+                .addClass("fade-out");
+                // .css("background-image",'url("assets/Impression_Sunrise.png")');
+        });
+
+    })
+}
+
+
+// update the painting page based on id
+function updatePainting(currentPainting){
+    let painting = currentPainting;
+    let imgElement = document.querySelector(".painting-full");
+    imgElement.src = "assets/"+painting['img'];
+
+    let textElementHeading = document.querySelector(".painting-container-text h3");
+    console.log(textElementHeading);
+    textElementHeading.innerText = painting['name'];
+    let textElementHeading2 = document.querySelector(".painting-container-text > h4");
+    textElementHeading2.innerText = painting['artist']+', '+painting['year'];    
+    let textElementParagraph = document.querySelector(".painting-container-text > p");
+    textElementParagraph.innerText = painting['intro'];
+
+    let artistImg = document.querySelector(".artist-profile");
+    artistImg.src = "assets/"+painting['artist_img'];
+
+    let artistName = document.querySelector(".artist-container-left > p");
+    artistName.innerHTML = painting['artist']+"<br>"+painting['artist_year'];
+
+    let artistIntro = document.querySelector(".artist-container-right > p");
+    artistIntro.innerText = painting['artist_intro'];
+}
+
+
+if(document.URL.includes("painting")){
+    // get parameter from URL
+    const queryString = window.location.search;
+    const params = new URLSearchParams(queryString);
+    const paintingID = params.get('id');
+
+    let currentPainting = paintings[paintingID];
+    console.log(currentPainting);
+    updatePainting(currentPainting);
+    html = document.querySelector("html");
+
+    //To do: relace with other colors using variables
+    html.style.backgroundColor = '#FCD9D9'; 
+
+
+    $(".painting-container-arrow").click(function(){
+
+        $("main")
+            .addClass("fadeOut");
+        $(".painting-container-arrow")
+            .addClass("fadeOut")
+            .bind("animationend webkitAnimationEnd oAnimationEnd MSAnimationEnd",
+            function() {
+                const transition_el = document.querySelector('.transition');
+                window.location.href = "movement.html";
+            });
+    })
+}
+    
+if(document.URL.includes("movement")){
+    const transition_el = document.querySelector('.transition');
+
+    // if user jump to this page from the painting page, then use the slide animation
+    if(document.referrer.includes("painting")){
+        transition_el.classList.add('is-active');
+        setTimeout(() => {
+            transition_el.classList.remove('is-active');
+        }, 500);   
+    }
+    
+    window.onload = () => {
+        const anchors = document.querySelectorAll('.painting-box a');
+            
+        for(let i=0; i<anchors.length; i++){
+            const anchor = anchors[i];
+            anchor.addEventListener('mouseover', e=>{
+                // let s = "#"+e.target.parentNode.id;
+                // let p = document.querySelector("#I1");
+                // p.classList.add('shadow');
+                anchor.classList.add('shadow');
+
+            });
+            anchor.addEventListener('mouseleave', e=>{
+                anchor.classList.remove('shadow');
+            });
+            anchor.addEventListener('click', e => {
+                e.preventDefault();    
+                console.log(anchor);
+
+
+        
+                transition_el.classList.add('is-active');
+                // console.log(anchor.href);
+                setInterval(() => {
+                    window.location.href = anchor.href;
+                //  window.location.href = "painting.html?id=I1";
+                },500);
+            });
+        }
+        
+}}
+
+
+
---
+++ project-test/js/painting_info.js
@@ -0,0 +1,83 @@
+const movements = {
+    "Neoclassicism":{
+        "img": "Oath_of_the_Horatii.jpeg",
+    },
+    "Romanticism":{
+        "img": "The_Raft_of_the_Medusa.jpeg"
+    },
+    "Realism":{
+        "img":"The_Gleaners.jpeg"
+    },
+    "Impressionism":{
+        "img":"Impression_Sunrise.png"
+    },
+    "Post-Impressionism":{
+        "img":"The_Starry_Night.jpeg"
+    },
+    "Symbolism":{
+        "img":"Death_and_Life.jpeg"
+    }
+}
+
+const paintings = {
+    "I1":{
+        "name": "Olympia",
+        "img": "Olympia.jpg",
+        "artist": "Édouard Manet",
+        "year": "1863",
+        "intro": `Olympia is a painting by Édouard Manet, first exhibited at the 1865 Paris Salon, which shows a nude woman ("Olympia") lying on a bed being brought flowers by a servant. Olympia was modelled by Victorine Meurent and Olympia's servant by the art model Laure. Olympia's confrontational gaze caused shock and astonishment when the painting was first exhibited because a number of details in the picture identified her as a prostitute. The French government acquired the painting in 1890 after a public subscription organized by Claude Monet. The painting is on display at the Musée d'Orsay, Paris.`,
+        "artist_year": "1832-1883",
+        "artist_img": "Manet.png",
+        "artist_intro": `He was one of the first 19th-century artists to paint modern life, as well as a pivotal figure in the transition from Realism to Impressionism.
+        Born into an upper-class household with strong political connections, Manet rejected the naval career originally envisioned for him; he became engrossed in the world of painting.`
+    },
+    "I2":{
+        "name": "Impression, Sunrise",
+        "img": "Impression_Sunrise.png",
+        "artist": "Claude Monet",
+        "year": "1872",
+        "intro": `Impression, Sunrise is first shown at what would become known as the "Exhibition of the Impressionists" in Paris in April, 1874. The painting is credited with inspiring the name of the Impressionist movement.
+        Monet depicts a mist, which provides a hazy background to the piece set in the French harbor. The orange and yellow hues contrast brilliantly with the dark vessels, where little, if any detail is immediately visible to the audience. It is a striking and candid work that shows the smaller boats in the foreground almost being propelled along by the movement of the water. This has, once again, been achieved by separate brushstrokes that also show various colors "sparkling" on the sea.`,
+        "artist_year": "1840-1926",
+        "artist_img": "Monet.png",
+        "artist_intro": `During his long career, Monet was the most consistent and prolific practitioner of impressionism's philosophy of expressing one's perceptions before nature, especially as applied to plein air (outdoor) landscape painting.`
+    },
+    "I3":{
+        "name": "The Cradle",
+        "img": "Cradle.jpg",
+        "artist": "Berthe Morisot",
+        "year": "1872",
+        "intro": `Morisot uses in this painting a reduced number of colors and a fluid brushstroke. The painting reflects an atmosphere of great intimacy, sweetness and protective love. This canvas, together with others like the one Claude Monet made of his son, Jean Monet at his Cradle (1867), attempt a new representation of childhood.
+        Morisot uses in this painting a reduced number of colors and a fluid brushstroke. The painting reflects an atmosphere of great intimacy, sweetness and protective love. This canvas, together with others like the one Claude Monet made of his son, Jean Monet at his Cradle (1867), attempt a new representation of childhood.`,
+        "artist_year": "1841-1895",
+        "artist_img": "Morisot.png",
+        "artist_intro": `Berthe Marie Pauline Morisot was a French painter and a member of the circle of painters in Paris who became known as the Impressionists. She was described by art critic Gustave Geffroy in 1894 as one of "les trois grandes dames" (The three great ladies) of Impressionism alongside Marie Bracquemond and Mary Cassatt.`
+    },
+    "I4":{
+        "name": "Luncheon of the Boating Party",
+        "img": "Luncheon_of_the_Boating_Party.jpg",
+        "artist": "Pierre-Auguste Renoir",
+        "year": "1880",
+        "intro": `The painting, combining figures, still-life, and landscape in one work, depicts a group of Renoir's friends relaxing on a balcony at the Maison Fournaise restaurant along the Seine river in Chatou, France. The painter and art patron, Gustave Caillebotte, is seated in the lower right. Renoir's future wife, Aline Charigot, is in the foreground playing with a small dog, an affenpinscher; she replaced an earlier woman who sat for the painting but with whom Renoir became annoyed. On the table is fruit and wine.
+        The painting is thought to show the influence of Italian Renaissance painter Paolo Veronese on Renoir's style, in particular, The Wedding Feast at Cana (1563), one of Renoir's favorite Veronese paintings at the Louvre, which depicts a similar banquet theme to that of the Luncheon.
+        `,
+        "artist_year": "1841-1919",
+        "artist_img": "Renoir.png",
+        "artist_intro": `Pierre-Auguste Renoir was a French artist who was a leading painter in the development of the Impressionist style. As a celebrator of beauty and especially feminine sensuality, it has been said that "Renoir is the final representative of a tradition which runs directly from Rubens to Watteau.”
+        `
+    },
+    "I5":{
+        "name": "Boulevard Montmartre, Mardi Gras",
+        "img": "Montmartre_Mardi_Gras.jpg",
+        "artist": "Camille Pissarro",
+        "year": "1897",
+        "intro":  `This work is part of a series of fourteen paintings depicting different times of the day and seasons of the Boulevard Montmartre in Paris. Camille Pissarro is known as the "Father of Impression" for his "teacher's eye" of drawing what he saw in front of him.
+        Pissarro depicts a nineteenth century Mardi Gras procession, also known as the Carnaval de Paris from the streets below extending its grandeur far into the distance. This street scene focuses on the idea of a "New Paris" that reveals an outdoor natural setting of beauty without adulteration in order to educate the public of real ideas. This painting marks one of Pissarro's last major works due to his weakening eyesight commonly associated with his elderly age. Many critics believe that this dis`,
+        "artist_year": "1830-1903",
+        "artist_img": "Pissarro.png",
+        "artist_intro": `His importance resides in his contributions to both Impressionism and Post-Impressionism.
+        Pissarro is the only artist to have shown his work at all eight Paris Impressionist exhibitions. He "acted as a father figure not only to the Impressionists" but to all four of the major Post-Impressionists, Cézanne, Seurat, Gauguin, and van Gogh.
+        `
+    },
+
+}
